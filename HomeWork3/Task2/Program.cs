679a5ad0e63a301a3f6377f2c71b6751e52016d6
Vitaly <m.v.a.-1992@mail.ru>
2022-12-26 03:15:39 +1000

    add Task 2

---
+++ HomeWork3/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите координаты первой точки:");
+Write("ax = ");
+int ax = int.Parse(ReadLine()!);
+Write("ay = ");
+int ay = int.Parse(ReadLine()!);
+Write("az = ");
+int az = int.Parse(ReadLine()!);
+
+WriteLine("Введите координаты второй точки:");
+Write("bx = ");
+int bx = int.Parse(ReadLine()!);
+Write("by = ");
+int by = int.Parse(ReadLine()!);
+Write("bz = ");
+int bz = int.Parse(ReadLine()!);
+
+int x = ax - bx;
+int y = ay - by;
+int z = az - bz;
+
+double result = Math.Sqrt((Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2)));
+
+Write($"Длина отрезка {result:f2}");
