2a984858bf86fe9d40780e8536ffabbd8d283945
Vitaly <m.v.a.-1992@mail.ru>
2022-12-26 03:15:49 +1000

    add Task 3

---
+++ HomeWork3/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+
+Clear();
+
+Write("Введите целое число: ");
+bool isParse = int.TryParse(ReadLine(), out int n);
+if (!isParse)
+{
+    Write("Ошибка! Введите число.");
+    return;
+}
+
+int i = 1;
+
+WriteLine($"Все числа в кубе от 1 до числа {n}: ", i);
+
+while (n > 0 ? i <= n : i >= n)
+{
+    if (n > 0)
+    {
+        Write($"| {i} = ");
+        Write($"{Math.Pow(i, 3)} |");
+        i++;
+    }
+    else
+    {
+        Write($"| {i} = ");
+        Write($"{Math.Pow(i, 3)} |");
+        --i;
+    }
+}
