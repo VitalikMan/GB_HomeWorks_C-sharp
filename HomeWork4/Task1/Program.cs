ed57d12eb208eb99fd0edf9d93fd3a95c54fd495
Vitaly <m.v.a.-1992@mail.ru>
2023-01-16 04:34:28 +1000

    add Task 1

---
+++ HomeWork4/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, реализующую метод, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B с использованием цикла.
+
+// В задаче не использовать стандартный метод Pow!
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4-> 16
+
+using static System.Console;
+
+Clear();
+
+Write("Введите число для возведения в степень: ");
+bool numberA = int.TryParse(ReadLine(), out int A);
+if (!numberA)
+{
+    WriteLine("Ошибка! Вы ввели не число!");
+    return;
+}
+
+Write($"Введите степень числа {A} для её расчёта: ");
+bool numberB = int.TryParse(ReadLine(), out int B);
+if (!numberB)
+{
+    WriteLine("Ошибка! Вы ввели не число!");
+    return;
+}
+
+int result = degreeOfNumber(A);
+
+if (A > 0)
+{
+    Write($"Число {A} в степени {B} равна {result}");
+}
+else
+{
+    Write($"Число {A} в степени {B} равна -{result}");
+}
+
+
+int degreeOfNumber(int A)
+{
+    int total = 1;
+    for (int i = 0; i < B; i++)
+    {
+        total *= A;
+    }
+    return total;
+}
