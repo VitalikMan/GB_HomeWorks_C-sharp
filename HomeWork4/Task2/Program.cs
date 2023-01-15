ed6279afece1fa3acaaf8e4f04fee8feb26bb566
Vitaly <m.v.a.-1992@mail.ru>
2023-01-16 04:34:36 +1000

    add Task 2

---
+++ HomeWork4/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+bool num = int.TryParse(ReadLine(), out int Number);
+if (!num)
+{
+    WriteLine("Ошибка! Вы ввели не число!");
+    return;
+}
+
+int result = SumDigitInNumber(Number);
+
+WriteLine($"Сумма цифр числа {Number} равна {result}");
+
+int SumDigitInNumber(int Number)
+{
+    int total = 0;
+    while (Number > 0)
+    {
+        total += (Number % 10);
+        Number /= 10;
+    }
+    return total;
+}
