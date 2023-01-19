356092a7cf4e5a37c1d5f2581f73ce7a32210ed5
Vitaly <m.v.a.-1992@mail.ru>
2023-01-19 18:33:36 +1000

    fixed Task 1

--- HomeWork4/Task1/Program.cs
+++ HomeWork4/Task1/Program.cs
@@ -27,24 +27,38 @@ if (!numberB)
     return;
 }
 
-int result = degreeOfNumber(A);
+// int degreeOfNumber(int A)    // <- Исходный вариант с недочётами в коде (использование В как глобальной переменной не очень хорошая практика. И в целом, лучше не использовать одинаковые имена вне и внутри методов..)
+// {
+//     int total = 1;
+//     for (int i = 0; i < B; i++)
+//     {
+//         total *= A;
+//     }
+//     return total;
+// }
 
-if (A > 0)
-{
-    Write($"Число {A} в степени {B} равна {result}");
-}
-else
-{
-    Write($"Число {A} в степени {B} равна -{result}");
-}
-
-
-int degreeOfNumber(int A)
+int degreeOfNumber(int num, int step)   // <- Исправленный вариант
 {
     int total = 1;
-    for (int i = 0; i < B; i++)
+    for (int i = 0; i < step; i++)
     {
-        total *= A;
+        total *= num;
     }
     return total;
 }
+
+int result = degreeOfNumber(A, B);
+
+void PrintDegreeOfNumber(int result)
+{
+    if (A > 0)
+    {
+        Write($"Число {A} в степени {B} равна {result}");
+    }
+    else
+    {
+        Write($"Число {A} в степени {B} равна -{result}");
+    }
+}
+
+PrintDegreeOfNumber(result);
