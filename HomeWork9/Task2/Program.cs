ce295a4ce9c62468ca68a53a92014d5463de9431
Vitaly <m.v.a.-1992@mail.ru>
2023-02-05 22:55:15 +1000

    add task 2

---
+++ HomeWork9/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// При решении задач обязательным условием является реализация рекурсивных методов
+
+// Напишите программу, которая реализует рекурсивный метод нахождения суммы
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.");
+WriteLine();
+Write("Введите первое и второе число через пробел: ");
+string[] par = ReadLine()!.Split(new string[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries);
+int M = int.Parse(par[0]);
+int N = int.Parse(par[1]);
+
+WriteLine();
+WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}:");
+WriteLine();
+Write($"M = {M}; N = {N} -> ");
+WriteLine(SumNumbersThroughRecursion(M, N));
+WriteLine();
+
+int SumNumbersThroughRecursion(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        int temp = numberM;
+        numberM = numberN;
+        numberN = temp;
+    }
+    if (numberM == numberN) return numberM;
+    else
+    {
+        return SumNumbersThroughRecursion(numberM + 1, numberN) + numberM;
+    }
+}
